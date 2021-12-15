482599647f6f519fc174cf61b10af19b43ed8f84
ShkarupyloMaksim <shkarupilo2005@gmail.com>
2021-12-15 16:37:02 +0200

    added 'show_var' and 'show_table'

--- main.py
+++ main.py
@@ -18,16 +18,16 @@ make_pdf = False
 photo_for_file = []
 
 
-
 @db.message_handler(commands=["start"])
 async def start(message: types.message):
     """The start method"""
     me = await bot.get_me()
     print(me)
     print(message)
-    await bot.send_message(message.chat.id, message.chat.last_name+' '+message.chat.first_name)
+    await bot.send_message(message.chat.id, message.chat.last_name + ' ' + message.chat.first_name)
     await message.answer('Я {} бот созданый с прихоти создателя для облегчения посылки дз\n Введите /help для '
                          'получениия большей информации'.format(me.first_name))
+
 
 @db.message_handler(commands=['add'])
 async def add(message: types.message):
@@ -35,21 +35,31 @@ async def add(message: types.message):
     message.text = message.text.replace("/add ", "")
     message.text = message.text.strip()
     data_base.add_info('queue', ['name'], [str(message.text)])
+
+
 @db.message_handler(commands=['delete'])
 async def delete(message: types.message):
     """The delete method"""
     message.text = message.text.replace("/delete ", "")
     message.text = message.text.strip()
     data_base.delete_info('queue', ['name'], [str(message.text)])
-@db.message_handler(commands=["queue"])
-async def help(message: types.message):
-    await message.answer('Привет {}, небольшая инструкция для получения тебе нужного pdf файла с картинок:\n1) Введи '
-                         'команду /home_task\n2) Отправь поочередно нужные фото\n3) Введи /end и подожди создания '
-                         'файла и его отправки тебе.'.format(message.chat.first_name))
-    # data_base.add_info('queue', 'name', message.)
-    print(message)
 
-#tr
+
+@db.message_handler(commands=['show_var'])
+async def show_variants(message: types.message):
+    """The show variants method"""
+    await message.answer(data_base.have_db('queue'))
+
+
+@db.message_handler(commands=['show_table'])
+async def show_table(message: types.message):
+    """The show table method"""
+    message.text = message.text.replace("/show_table ", "")
+    message.text = message.text.strip()
+    data = data_base.return_info('queue', [str(message.text)])  # not sure about arguments
+    await message.answer(data)
+
+
+# tr
 if __name__ == '__main__':
     executor.start_polling(db, skip_updates=True)
-
