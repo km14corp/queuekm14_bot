c5730f2c5979b6c7ad7243e6ce812f440e68e7da
ShkarupyloMaksim <shkarupilo2005@gmail.com>
2021-12-16 23:35:04 +0200

    queue class was added, it made to make your life easier

---
+++ queue.py
@@ -0,0 +1,25 @@
+from Data_base.db_help_class import db_help
+
+
+class queue:
+    """This class for making your life easier, so you can use it to operate with queue with the 'name' how an object
+    with it`s functions """
+    def __init__(self, name):
+        """The init method which create table with 'name' if it not exists"""
+        self.name = name
+        self.data_base = db_help('Data_base\\queue.db')
+        self.data_base.make_db(self.name)
+
+    def __del__(self):
+        """The initialisation of __del__ function to del table with it`s object"""
+        self.data_base.del_db(self.name)
+        del self
+
+    def return_names(self):
+        """This method will return list of names of this object"""
+        self.data_base.return_names(self.name)
+
+    def add_id(self, id):
+        """This method is to add new person into the objects table
+        - id - the persons tg id"""
+        self.data_base.add_info(self.name, 'name', id)
