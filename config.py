0db22e9631db7d58968d18736608502d1a07bcad
Liza Mandryko <prostlizok@gmail.com>
2022-01-10 22:45:02 +0200

    new schedule commit

--- config.py
+++ config.py
@@ -1,4 +1,1 @@
-# from decouple import config
-TOKEN = '5010712495:AAH-xNg3NRO4G0hkFkY_JPqTXt-gubmdbDY'
-url = "http://rozklad.kpi.ua/Schedules/ViewSchedule.aspx?g=8bb9bcf6-5db2-4124-8c1a-d0debc152bc9"
-# TOKEN = config('TOKEN')
+TOKEN = config('TOKEN')
